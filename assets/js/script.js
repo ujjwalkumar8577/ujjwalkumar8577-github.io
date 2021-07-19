d72d6e2c97cea073181e8e6f2f69cd0417c6ab3b
Ujjwal Kumar <ujjwalkumar8577@gmail.com>
2021-07-19 20:31:15 +0530

    Improved project section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // Sticky navbar
 $(document).ready(function () {
 
+    var projects = null;
+    $.getJSON("project.json", function(json) {
+        projects = json;
+        console.log(projects);
+    });
+
     link1 = document.getElementById('link1');
     link2 = document.getElementById('link2');
     link3 = document.getElementById('link3');
@@ -77,4 +83,36 @@ $(document).ready(function () {
         // On page load
         stickyToggle(sticky, stickyWrapper, $(window));
     });
+
+    var closeButton = document.getElementById("close-project");
+    var project = document.getElementById("project");
+
+    closeButton.onclick = function() {
+        project.style.visibility = "hidden";
+    }
+
+    project1.onclick = function() {        
+        var p = projects["projects"][0];
+        loadProject(p);
+    }
+    
+    project2.onclick = function() {        
+        var p = projects["projects"][1];
+        loadProject(p);
+    }
+
+    project3.onclick = function() {        
+        var p = projects["projects"][2];
+        loadProject(p);
+    }
+
+    function loadProject(p) {
+        document.getElementById("project-title").innerHTML = p["title"];
+        document.getElementById("project-img").src = p["img"];
+        document.getElementById("project-desc1").innerHTML = p["desc1"];
+        document.getElementById("project-desc2").innerHTML = p["desc2"];
+        document.getElementById("project-github").href = p["github"];
+        document.getElementById("project-demo").href = p["demo"];
+        project.style.visibility = "visible";
+    }
 });
